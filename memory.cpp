965a4fc82af5f1b4655457258585132d105933f5
Will Hawkins <hawkinsw@borlaugic.com>
2018-08-17 01:26:47 -0400

    Refactor the memory space code.

--- memory.cpp
+++ memory.cpp
@@ -5,20 +5,36 @@ using namespace std;
 MemorySpace::MemorySpace(const MemorySpace &ms) {
 	m_initializer = ms.m_initializer;
 
-	for (auto mse : m_space) {
-		m_space[mse.first] = std::make_shared<MemoryValue>(*mse.second);
+	for (auto mse : ms.m_memory) {
+		m_memory[mse.first] = std::make_shared<MemoryValue>(*mse.second);
+	}
+	for (auto rse : ms.m_registers) {
+		m_registers[rse.first] = std::make_shared<RegisterValue>(*rse.second);
 	}
 }
 
-bool MemorySpace::GetValue(MemoryAddress addr, MemoryValue &value) {
-	if (m_space[addr]) {
-		value = *m_space[addr];
+bool MemorySpace::GetMemoryValue(MemoryAddress_t addr, MemoryValue &value) {
+	if (m_memory.end() != m_memory.find(addr)) {
+		value = *m_memory[addr];
 		return true;
 	}
 	return false;
 }
 
-void MemorySpace::SetValue(const MemoryValue &value) {
-	m_space[value.GetLocation()] = std::make_shared<MemoryValue>(value);
+void MemorySpace::SetMemoryValue(const MemoryValue &value) {
+	m_memory[value.GetLocation()] = std::make_shared<MemoryValue>(value);
+	return;
+}
+
+bool MemorySpace::GetRegisterValue(RegisterAddress_t addr, RegisterValue &value) {
+	if (m_registers.end() != m_registers.find(addr)) {
+		value = *m_registers[addr];
+		return true;
+	}
+	return false;
+}
+
+void MemorySpace::SetRegisterValue(const RegisterValue &value) {
+	m_registers[value.GetLocation()] = std::make_shared<RegisterValue>(value);
 	return;
 }
--- memory.hpp
+++ memory.hpp
@@ -6,29 +6,32 @@
 #include <map>
 #include <memory>
 
-typedef uint64_t MemoryAddress;
+typedef uint64_t ValueContents_t;
+typedef uint64_t ValueAddress_t;
+typedef uint64_t MemoryAddress_t;
+typedef uint64_t RegisterAddress_t;
 
-class MemoryValue
+class Value
 {
 	public:
-		MemoryValue() :
+		Value() :
 			m_value(0),
 			m_tainted(false),
 			m_location(0) {}
-		MemoryValue(MemoryAddress location) :
+		Value(ValueAddress_t location) :
 			m_value(0),
 			m_tainted(false),
 			m_location(location) {}
-		MemoryValue(const MemoryValue &v) : 
+		Value(const Value &v) : 
 			m_value(v.GetValue()),
 			m_tainted(v.GetTainted()),
 			m_location(v.GetLocation()) {}
 
-		virtual uint32_t GetValue() const {
+		virtual ValueContents_t GetValue() const {
 			return m_value;
 		}
 
-		virtual MemoryAddress GetLocation() const {
+		virtual ValueAddress_t GetLocation() const {
 			return m_location;
 		}
 
@@ -36,7 +39,7 @@ class MemoryValue
 			return m_tainted;
 		}
 
-		virtual void SetValue(uint32_t value) {
+		virtual void SetValue(ValueContents_t value) {
 			m_value = value;
 		}
 		
@@ -44,41 +47,57 @@ class MemoryValue
 			m_tainted = tainted;
 		}
 
-		virtual void SetLocation(const MemoryAddress location) {
+		virtual void SetLocation(const ValueAddress_t location) {
 			m_location = location;
 		}
 
-		~MemoryValue() { }
+		~Value() { }
 
 	private:
-		uint32_t m_value;
+		ValueContents_t m_value;
 		bool m_tainted;
-		MemoryAddress m_location;
+		ValueAddress_t m_location;
 
 
 };
 
-class RegisterMemoryValue : MemoryValue
+class RegisterValue : public Value
 {
+  using Value::Value;
+  public:
+    static constexpr ValueContents_t DefaultRegisterValue() { 
+	return 0x11111100UL;
+    }
 };
 
-class MemoryMemoryValue : MemoryValue
+class MemoryValue : public Value
 {
+  using Value::Value;
 };
 
 class MemorySpace
 {
 	public:
-		MemorySpace(uint64_t initializer) : m_initializer(initializer) {};
+		MemorySpace(uint32_t initializer) : m_initializer(initializer) {};
 		MemorySpace(const MemorySpace &ms);
 
-		bool GetValue(MemoryAddress addr, MemoryValue &value);
-		void SetValue(const MemoryValue &value);
+		uint32_t GetInitializer() {
+		    return m_initializer++;
+		}
+		bool GetMemoryValue(MemoryAddress_t addr, MemoryValue &value);
+		void SetMemoryValue(const MemoryValue &value);
+
+		bool GetRegisterValue(RegisterAddress_t addr, RegisterValue
+				      &value);
+		void SetRegisterValue(const RegisterValue &value);
 
 		~MemorySpace() { }
 	private:
 		uint64_t m_initializer;
-		std::map<MemoryAddress, std::shared_ptr<MemoryValue>> m_space;
+		std::map<MemoryAddress_t, std::shared_ptr<MemoryValue>> 
+		  m_memory;
+		std::map<RegisterAddress_t, std::shared_ptr<RegisterValue>> 
+		  m_registers;
 };
 
 #endif
