6e2e6dba7bc852517ecae73472cc0c69cedad403
Will Hawkins <hawkinsw@borlaugic.com>
2018-08-20 00:09:55 -0400

    reformat memory classes and add operator<< to MemoryValue.

--- memory.cpp
+++ memory.cpp
@@ -3,38 +3,54 @@
 using namespace std;
 
 MemorySpace::MemorySpace(const MemorySpace &ms) {
-	m_initializer = ms.m_initializer;
+    m_initializer = ms.m_initializer;
 
-	for (auto mse : ms.m_memory) {
-		m_memory[mse.first] = std::make_shared<MemoryValue>(*mse.second);
-	}
-	for (auto rse : ms.m_registers) {
-		m_registers[rse.first] = std::make_shared<RegisterValue>(*rse.second);
-	}
+    for (auto mse : ms.m_memory) {
+	m_memory[mse.first] = std::make_shared<MemoryValue>(*mse.second);
+    }
+    for (auto rse : ms.m_registers) {
+	m_registers[rse.first] = std::make_shared<RegisterValue>(*rse.second);
+    }
 }
 
-bool MemorySpace::GetMemoryValue(MemoryAddress_t addr, MemoryValue &value) {
-	if (m_memory.end() != m_memory.find(addr)) {
-		value = *m_memory[addr];
-		return true;
-	}
-	return false;
+bool MemorySpace::GetMemoryValue(MemoryValue &value) {
+    if (m_memory.end() != m_memory.find(value.GetLocation())) {
+	value = *m_memory[value.GetLocation()];
+	cout << "Get: "<<  std::hex << value.GetLocation() << ": "<<std::hex<<
+	  value.GetValue() << "." << endl;
+	return true;
+    }
+    return false;
 }
 
 void MemorySpace::SetMemoryValue(const MemoryValue &value) {
-	m_memory[value.GetLocation()] = std::make_shared<MemoryValue>(value);
-	return;
+    cout << "Set: "<<  std::hex << value.GetLocation() << ": " << std::hex <<
+      value.GetValue() << "." << endl;
+    m_memory[value.GetLocation()] = std::make_shared<MemoryValue>(value);
+    return;
 }
 
-bool MemorySpace::GetRegisterValue(RegisterAddress_t addr, RegisterValue &value) {
-	if (m_registers.end() != m_registers.find(addr)) {
-		value = *m_registers[addr];
-		return true;
-	}
-	return false;
+bool MemorySpace::GetRegisterValue(RegisterValue &value) {
+    if (m_registers.end() != m_registers.find(value.GetLocation())) {
+	value = *m_registers[value.GetLocation()];
+	return true;
+    }
+    return false;
 }
 
 void MemorySpace::SetRegisterValue(const RegisterValue &value) {
-	m_registers[value.GetLocation()] = std::make_shared<RegisterValue>(value);
-	return;
+    m_registers[value.GetLocation()]=std::make_shared<RegisterValue>(value);
+    return;
+}
+
+ostream& operator<<(ostream &os, const Value &mv) {
+    os << "Value: " << std::hex
+      << mv.GetValue()
+      << " at " << std::hex
+      << mv.GetLocation();
+    if (mv.GetTainted()) {
+	os << " (tainted)";
+    }
+    os << ".";
+    return os;
 }
--- memory.hpp
+++ memory.hpp
@@ -13,50 +13,63 @@ typedef uint64_t RegisterAddress_t;
 
 class Value
 {
-	public:
-		Value() :
-			m_value(0),
-			m_tainted(false),
-			m_location(0) {}
-		Value(ValueAddress_t location) :
-			m_value(0),
-			m_tainted(false),
-			m_location(location) {}
-		Value(const Value &v) : 
-			m_value(v.GetValue()),
-			m_tainted(v.GetTainted()),
-			m_location(v.GetLocation()) {}
+public:
+  Value() :
+    m_value(0),
+    m_tainted(false),
+    m_location(0) {}
 
-		virtual ValueContents_t GetValue() const {
-			return m_value;
-		}
+  Value(ValueAddress_t location) :
+    m_value(0),
+    m_tainted(false),
+    m_location(location) {}
+  Value(ValueAddress_t location,
+	ValueContents_t value) :
+    m_value(value),
+    m_tainted(false),
+    m_location(location) {}
+  Value(ValueAddress_t location,
+	ValueContents_t value,
+	bool tainted) :
+    m_value(value),
+    m_tainted(tainted),
+    m_location(location) {}
 
-		virtual ValueAddress_t GetLocation() const {
-			return m_location;
-		}
+  Value(const Value &v) :
+    m_value(v.GetValue()),
+    m_tainted(v.GetTainted()),
+    m_location(v.GetLocation()) {}
 
-		virtual bool GetTainted() const {
-			return m_tainted;
-		}
+  virtual ValueContents_t GetValue() const {
+      return m_value;
+  }
 
-		virtual void SetValue(ValueContents_t value) {
-			m_value = value;
-		}
-		
-		virtual void SetTainted(bool tainted) {
-			m_tainted = tainted;
-		}
+  virtual ValueAddress_t GetLocation() const {
+      return m_location;
+  }
 
-		virtual void SetLocation(const ValueAddress_t location) {
-			m_location = location;
-		}
+  virtual bool GetTainted() const {
+      return m_tainted;
+  }
 
-		~Value() { }
+  virtual void SetValue(ValueContents_t value) {
+      m_value = value;
+  }
 
-	private:
-		ValueContents_t m_value;
-		bool m_tainted;
-		ValueAddress_t m_location;
+  virtual void SetTainted(bool tainted) {
+      m_tainted = tainted;
+  }
+
+  virtual void SetLocation(const ValueAddress_t location) {
+      m_location = location;
+  }
+
+  ~Value() { }
+
+private:
+  ValueContents_t m_value;
+  bool m_tainted;
+  ValueAddress_t m_location;
 
 
 };
@@ -64,10 +77,10 @@ class Value
 class RegisterValue : public Value
 {
   using Value::Value;
-  public:
-    static constexpr ValueContents_t DefaultRegisterValue() { 
-	return 0x11111100UL;
-    }
+public:
+  static constexpr ValueContents_t DefaultRegisterValue() {
+      return 0x11111100UL;
+  }
 };
 
 class MemoryValue : public Value
@@ -77,27 +90,27 @@ class MemoryValue : public Value
 
 class MemorySpace
 {
-	public:
-		MemorySpace(uint32_t initializer) : m_initializer(initializer) {};
-		MemorySpace(const MemorySpace &ms);
+public:
+  MemorySpace(uint32_t initializer) : m_initializer(initializer) {};
+  MemorySpace(const MemorySpace &ms);
 
-		uint32_t GetInitializer() {
-		    return m_initializer++;
-		}
-		bool GetMemoryValue(MemoryAddress_t addr, MemoryValue &value);
-		void SetMemoryValue(const MemoryValue &value);
+  uint32_t GetInitializer() {
+      return m_initializer++;
+  }
+  bool GetMemoryValue(MemoryValue &value);
+  void SetMemoryValue(const MemoryValue &value);
 
-		bool GetRegisterValue(RegisterAddress_t addr, RegisterValue
-				      &value);
-		void SetRegisterValue(const RegisterValue &value);
+  bool GetRegisterValue(RegisterValue &value);
+  void SetRegisterValue(const RegisterValue &value);
 
-		~MemorySpace() { }
-	private:
-		uint64_t m_initializer;
-		std::map<MemoryAddress_t, std::shared_ptr<MemoryValue>> 
-		  m_memory;
-		std::map<RegisterAddress_t, std::shared_ptr<RegisterValue>> 
-		  m_registers;
+  ~MemorySpace() { }
+private:
+  uint64_t m_initializer;
+  std::map<MemoryAddress_t, std::shared_ptr<MemoryValue>>
+    m_memory;
+  std::map<RegisterAddress_t, std::shared_ptr<RegisterValue>>
+    m_registers;
 };
 
+std::ostream& operator<<(std::ostream &, const Value &);
 #endif
